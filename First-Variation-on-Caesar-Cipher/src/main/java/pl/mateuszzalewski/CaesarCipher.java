922358afc47c1ac37d2c381cc1d92fcac33a90a3
matzal <mati.zalewski@gmail.com>
2018-05-19 20:14:41 +0200

    ceasar cipher - initial commit

---
+++ First-Variation-on-Caesar-Cipher/src/main/java/pl/mateuszzalewski/CaesarCipher.java
@@ -0,0 +1,14 @@
+package pl.mateuszzalewski;
+
+import java.util.List;
+
+public class CaesarCipher {
+    public static List<String>  movingShift(String s, int shift) {
+		return null;
+    }
+
+    public static String  demovingShift(List<String> s, int shift) {
+		return null;
+    }
+
+}
