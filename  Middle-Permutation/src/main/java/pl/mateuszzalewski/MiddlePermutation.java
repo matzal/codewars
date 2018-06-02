2e76a8a42fc71fc070e65f453b7ecf8d2fe54d2b
matzal <mati.zalewski@gmail.com>
2018-06-02 16:34:36 +0200

    middle permutation - initial commit

---
+++  Middle-Permutation/src/main/java/pl/mateuszzalewski/MiddlePermutation.java
@@ -0,0 +1,9 @@
+package pl.mateuszzalewski;
+
+public class MiddlePermutation {
+
+    public static String findMidPerm(String strng) {
+        // your code here!
+        return "";
+    }
+}
---
+++  Middle-Permutation/src/test/java/pl/mateuszzalewski/SolutionTest.java
@@ -0,0 +1,16 @@
+package pl.mateuszzalewski;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void basicTests() {
+        assertEquals("bac", MiddlePermutation.findMidPerm("abc"));
+        assertEquals("bdca", MiddlePermutation.findMidPerm("abcd"));
+        assertEquals("cbxda", MiddlePermutation.findMidPerm("abcdx"));
+        assertEquals("cxgdba", MiddlePermutation.findMidPerm("abcdxg"));
+        assertEquals("dczxgba", MiddlePermutation.findMidPerm("abcdxgz"));
+    }
+}
