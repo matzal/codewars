303979913b1cf718a4c804d7c48703e3e10087f5
matzal <mati.zalewski@gmail.com>
2018-06-02 19:12:43 +0200

    middle permutation - implement class and pass tests

---  Middle-Permutation/src/main/java/pl/mateuszzalewski/MiddlePermutation.java
+++  Middle-Permutation/src/main/java/pl/mateuszzalewski/MiddlePermutation.java
@@ -1,9 +1,26 @@
 package pl.mateuszzalewski;
 
+import java.util.Arrays;
+
 public class MiddlePermutation {
 
     public static String findMidPerm(String strng) {
-        // your code here!
-        return "";
+        char[] chars = strng.toCharArray();
+        Arrays.sort(chars);
+        int length = strng.length();
+        int middle = (length - 1) / 2;
+        String remainder = new StringBuilder()
+                .append(chars)
+                .deleteCharAt(middle)
+                .toString();
+        if (length % 2 == 0) {
+            return new StringBuilder()
+                    .append(remainder)
+                    .append(chars[middle])
+                    .reverse()
+                    .toString();
+        } else {
+            return chars[middle] + findMidPerm(remainder);
+        }
     }
 }
---  Middle-Permutation/src/test/java/pl/mateuszzalewski/SolutionTest.java
+++  Middle-Permutation/src/test/java/pl/mateuszzalewski/SolutionTest.java
@@ -2,7 +2,6 @@ package pl.mateuszzalewski;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class SolutionTest {
     @Test
